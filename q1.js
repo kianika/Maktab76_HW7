4ca18e7f9fff67fc603b77ab1419ba3a8508770d
kianika <mona_yaghoubi@gmail.com>
2022-05-10 22:40:27 +0200

    v.1 updated

---
+++ q1.js
@@ -0,0 +1,16 @@
+function calcCircle(r){
+    this.r = r;
+    
+    this.getArea = function(){
+
+        return Math.PI*Math.pow(r, 2);
+    }
+    this.getPerimeter = function(){
+        return Math.PI*2*r;
+    }
+}
+
+let circle = new calcCircle(4);
+console.log(circle.getArea());
+console.log(circle.getPerimeter());
+
