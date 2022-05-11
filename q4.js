cd40425b95db841347b524ce443640adf5200a21
kianika <mona_yaghoubi@gmail.com>
2022-05-12 01:59:58 +0200

    v2

--- q4.js
+++ q4.js
@@ -1,6 +1,27 @@
+//Approach 1
+function listInt(array){
+    let result = [];
+    for(item of array){
+        let s = item.split("");
+        for(n of s){
+            if(Number.isInteger(parseInt(n))){
+                result.push(item);
+                break;
+            }
+        }
+    }
+    return result;
+}
 let arr = ["g3jg", "hghg", "gfdfdf7"];
+console.log(listInt(arr));
 
-let ifNumber = (str) => str.filter(x => /[0-9]/.test(x));  
+
+
+
+//Approach 2
+/* let arr = ["g3jg", "hghg", "gfdfdf7"];
+
+let listInt = (str) => str.filter(x => /[0-9]/.test(x));  
  
-console.log(ifNumber(arr)); 
+console.log(listInt(arr));  */
 
